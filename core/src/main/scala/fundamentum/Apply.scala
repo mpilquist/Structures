18720503096c4f8cf963c1eea07636c9593e9799
Michael Pilquist <mpilquist@gmail.com>
2015-01-21 23:01:55 -0500

    Suppress syntax creation for Apply methods

--- core/src/main/scala/fundamentum/Apply.scala
+++ core/src/main/scala/fundamentum/Apply.scala
@@ -1,6 +1,6 @@
 package fundamentum
 
-import simulacrum.typeclass
+import simulacrum.{ typeclass, noop }
 
 /**
  * Describes type constructors that are functors and that support the `apply` method such that:
@@ -16,91 +16,91 @@ import simulacrum.typeclass
  */
 @typeclass trait Apply[F[_]] extends Functor[F] {
 
-  def apply[A, B](fa: F[A])(f: F[A => B]): F[B]
+  @noop def apply[A, B](fa: F[A])(f: F[A => B]): F[B]
 
 
-  def apply2[A, B, X](fa: F[A], fb: F[B])(f: F[(A, B) => X]): F[X] =
+  @noop def apply2[A, B, X](fa: F[A], fb: F[B])(f: F[(A, B) => X]): F[X] =
     apply(fa)(apply(fb)(map(f)(ff => (b: B) => (a: A) => ff(a, b))))
 
-  def apply3[A, B, C, X](fa: F[A], fb: F[B], fc: F[C])(f: F[(A, B, C) => X]): F[X] =
+  @noop def apply3[A, B, C, X](fa: F[A], fb: F[B], fc: F[C])(f: F[(A, B, C) => X]): F[X] =
     apply(fa)(apply2(fb, fc)(map(f)(ff => (b: B, c: C) => (a: A) => ff(a, b, c))))
 
-  def apply4[A, B, C, D, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: F[(A, B, C, D) => X]): F[X] =
+  @noop def apply4[A, B, C, D, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: F[(A, B, C, D) => X]): F[X] =
     apply2(fa, fb)(apply2(fc, fd)(map(f)(ff => (c: C, d: D) => (a: A, b: B) => ff(a, b, c, d))))
 
-  def apply5[A, B, C, D, E, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E])(f: F[(A, B, C, D, E) => X]): F[X] =
+  @noop def apply5[A, B, C, D, E, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E])(f: F[(A, B, C, D, E) => X]): F[X] =
     apply2(fa, fb)(apply3(fc, fd, fe)(map(f)(ff => (c: C, d: D, e: E) => (a: A, b: B) => ff(a, b, c, d, e))))
 
-  def apply6[A, B, C, D, E, G, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G])(f: F[(A, B, C, D, E, G) => X]): F[X] =
+  @noop def apply6[A, B, C, D, E, G, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G])(f: F[(A, B, C, D, E, G) => X]): F[X] =
     apply3(fa, fb, fc)(apply3(fd, fe, fg)(map(f)(ff => (d: D, e: E, g: G) => (a: A, b: B, c: C) => ff(a, b, c, d, e, g))))
 
-  def apply7[A, B, C, D, E, G, H, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H])(f: F[(A, B, C, D, E, G, H) => X]): F[X] =
+  @noop def apply7[A, B, C, D, E, G, H, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H])(f: F[(A, B, C, D, E, G, H) => X]): F[X] =
     apply3(fa, fb, fc)(apply4(fd, fe, fg, fh)(map(f)(ff => (d: D, e: E, g: G, h: H) => (a: A, b: B, c: C) => ff(a, b, c, d, e, g, h))))
 
-  def apply8[A, B, C, D, E, G, H, I, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I])(f: F[(A, B, C, D, E, G, H, I) => X]): F[X] =
+  @noop def apply8[A, B, C, D, E, G, H, I, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I])(f: F[(A, B, C, D, E, G, H, I) => X]): F[X] =
     apply4(fa, fb, fc, fd)(apply4(fe, fg, fh, fi)(map(f)(ff => (e: E, g: G, h: H, i: I) => (a: A, b: B, c: C, d: D) => ff(a, b, c, d, e, g, h, i))))
 
-  def apply9[A, B, C, D, E, G, H, I, J, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J])(f: F[(A, B, C, D, E, G, H, I, J) => X]): F[X] =
+  @noop def apply9[A, B, C, D, E, G, H, I, J, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J])(f: F[(A, B, C, D, E, G, H, I, J) => X]): F[X] =
     apply4(fa, fb, fc, fd)(apply5(fe, fg, fh, fi, fj)(map(f)(ff => (e: E, g: G, h: H, i: I, j: J) => (a: A, b: B, c: C, d: D) => ff(a, b, c, d, e, g, h, i, j))))
 
-  def apply10[A, B, C, D, E, G, H, I, J, K, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K])(f: F[(A, B, C, D, E, G, H, I, J, K) => X]): F[X] =
+  @noop def apply10[A, B, C, D, E, G, H, I, J, K, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K])(f: F[(A, B, C, D, E, G, H, I, J, K) => X]): F[X] =
     apply5(fa, fb, fc, fd, fe)(apply5(fg, fh, fi, fj, fk)(map(f)(ff => (g: G, h: H, i: I, j: J, k: K) => (a: A, b: B, c: C, d: D, e: E) => ff(a, b, c, d, e, g, h, i, j, k))))
 
 
 
-  def map2[A, B, X](fa: F[A], fb: F[B])(f: (A, B) => X): F[X] =
+  @noop def map2[A, B, X](fa: F[A], fb: F[B])(f: (A, B) => X): F[X] =
     apply(fa)(map(fb)(b => (a: A) => f(a, b)))
 
-  def map3[A, B, C, X](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => X): F[X] =
+  @noop def map3[A, B, C, X](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => X): F[X] =
     apply(fa)(map2(fb, fc)((b, c) => a => f(a, b, c)))
 
-  def map4[A, B, C, D, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => X): F[X] =
+  @noop def map4[A, B, C, D, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => X): F[X] =
     map2(tuple2(fa, fb), tuple2(fc, fd)) { case ((a, b), (c, d)) => f(a, b, c, d) }
 
-  def map5[A, B, C, D, E, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E])(f: (A, B, C, D, E) => X): F[X] =
+  @noop def map5[A, B, C, D, E, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E])(f: (A, B, C, D, E) => X): F[X] =
     map2(tuple2(fa, fb), tuple3(fc, fd, fe)) { case ((a, b), (c, d, e)) => f(a, b, c, d, e) }
 
-  def map6[A, B, C, D, E, G, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G])(f: (A, B, C, D, E, G) => X): F[X] =
+  @noop def map6[A, B, C, D, E, G, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G])(f: (A, B, C, D, E, G) => X): F[X] =
     map2(tuple3(fa, fb, fc), tuple3(fd, fe, fg)) { case ((a, b, c), (d, e, g)) => f(a, b, c, d, e, g) }
 
-  def map7[A, B, C, D, E, G, H, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H])(f: (A, B, C, D, E, G, H) => X): F[X] =
+  @noop def map7[A, B, C, D, E, G, H, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H])(f: (A, B, C, D, E, G, H) => X): F[X] =
     map2(tuple3(fa, fb, fc), tuple4(fd, fe, fg, fh)) { case ((a, b, c), (d, e, g, h)) => f(a, b, c, d, e, g, h) }
 
-  def map8[A, B, C, D, E, G, H, I, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I])(f: (A, B, C, D, E, G, H, I) => X): F[X] =
+  @noop def map8[A, B, C, D, E, G, H, I, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I])(f: (A, B, C, D, E, G, H, I) => X): F[X] =
     map2(tuple4(fa, fb, fc, fd), tuple4(fe, fg, fh, fi)) { case ((a, b, c, d), (e, g, h, i)) => f(a, b, c, d, e, g, h, i) }
 
-  def map9[A, B, C, D, E, G, H, I, J, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J])(f: (A, B, C, D, E, G, H, I, J) => X): F[X] =
+  @noop def map9[A, B, C, D, E, G, H, I, J, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J])(f: (A, B, C, D, E, G, H, I, J) => X): F[X] =
     map2(tuple4(fa, fb, fc, fd), tuple5(fe, fg, fh, fi, fj)) { case ((a, b, c, d), (e, g, h, i, j)) => f(a, b, c, d, e, g, h, i, j) }
 
-  def map10[A, B, C, D, E, G, H, I, J, K, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K])(f: (A, B, C, D, E, G, H, I, J, K) => X): F[X] =
+  @noop def map10[A, B, C, D, E, G, H, I, J, K, X](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K])(f: (A, B, C, D, E, G, H, I, J, K) => X): F[X] =
     map2(tuple5(fa, fb, fc, fd, fe), tuple5(fg, fh, fi, fj, fk)) { case ((a, b, c, d, e), (g, h, i, j, k)) => f(a, b, c, d, e, g, h, i, j, k) }
 
 
 
-  def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+  @noop def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
     map2(fa, fb)((_, _))
 
-  def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] =
+  @noop def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] =
     map3(fa, fb, fc)((_, _, _))
 
-  def tuple4[A, B, C, D](fa: F[A], fb: F[B], fc: F[C], fd: F[D]): F[(A, B, C, D)] =
+  @noop def tuple4[A, B, C, D](fa: F[A], fb: F[B], fc: F[C], fd: F[D]): F[(A, B, C, D)] =
     map4(fa, fb, fc, fd)((_, _, _, _))
 
-  def tuple5[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E]): F[(A, B, C, D, E)] =
+  @noop def tuple5[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E]): F[(A, B, C, D, E)] =
     map5(fa, fb, fc, fd, fe)((_, _, _, _, _))
 
-  def tuple6[A, B, C, D, E, G](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G]): F[(A, B, C, D, E, G)] =
+  @noop def tuple6[A, B, C, D, E, G](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G]): F[(A, B, C, D, E, G)] =
     map6(fa, fb, fc, fd, fe, fg)((_, _, _, _, _, _))
 
-  def tuple7[A, B, C, D, E, G, H](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H]): F[(A, B, C, D, E, G, H)] =
+  @noop def tuple7[A, B, C, D, E, G, H](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H]): F[(A, B, C, D, E, G, H)] =
     map7(fa, fb, fc, fd, fe, fg, fh)((_, _, _, _, _, _, _))
 
-  def tuple8[A, B, C, D, E, G, H, I](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I]): F[(A, B, C, D, E, G, H, I)] =
+  @noop def tuple8[A, B, C, D, E, G, H, I](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I]): F[(A, B, C, D, E, G, H, I)] =
     map8(fa, fb, fc, fd, fe, fg, fh, fi)((_, _, _, _, _, _, _, _))
 
-  def tuple9[A, B, C, D, E, G, H, I, J](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J]): F[(A, B, C, D, E, G, H, I, J)] =
+  @noop def tuple9[A, B, C, D, E, G, H, I, J](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J]): F[(A, B, C, D, E, G, H, I, J)] =
     map9(fa, fb, fc, fd, fe, fg, fh, fi, fj)((_, _, _, _, _, _, _, _, _))
 
-  def tuple10[A, B, C, D, E, G, H, I, J, K](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K]): F[(A, B, C, D, E, G, H, I, J, K)] =
+  @noop def tuple10[A, B, C, D, E, G, H, I, J, K](fa: F[A], fb: F[B], fc: F[C], fd: F[D], fe: F[E], fg: F[G], fh: F[H], fi: F[I], fj: F[J], fk: F[K]): F[(A, B, C, D, E, G, H, I, J, K)] =
     map10(fa, fb, fc, fd, fe, fg, fh, fi, fj, fk)((_, _, _, _, _, _, _, _, _, _))
 }
