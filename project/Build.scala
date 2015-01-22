ef69d7e173ac18093bcb43075ffb53857f4867b9
Michael Pilquist <mpilquist@gmail.com>
2015-01-21 22:19:07 -0500

    Build cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -50,6 +50,8 @@ object FundamentumBuild extends Build {
     settings(
       name := "fundamentum-laws",
       libraryDependencies ++= Seq(
+        "org.scalacheck" %% "scalacheck" % "1.11.3",
+        "org.typelevel" %% "discipline" % "0.2.1",
         "org.scalatest" %% "scalatest" % "2.2.3" % "test"
       )
     )
