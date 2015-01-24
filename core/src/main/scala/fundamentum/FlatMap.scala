0ff68d1af26cf2ea004a73f8f18546472e8949be
Michael Pilquist <mpilquist@gmail.com>
2015-01-23 19:11:12 -0500

    Remove >>

--- core/src/main/scala/fundamentum/FlatMap.scala
+++ core/src/main/scala/fundamentum/FlatMap.scala
@@ -7,10 +7,6 @@ import simulacrum.{ noop, op, typeclass }
   @op(">>=", alias = true)
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
-  @op(">>", alias = true)
-  def flatAs[A, B](fa: F[A], fb: F[B]): F[B] =
-    flatMap(fa)(_ => fb)
-
   def flatten[A, B](ffa: F[F[A]]): F[A] =
     flatMap(ffa)(identity)
 
