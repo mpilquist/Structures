925b593969f3f5d9150cf08f3dde18bfe6cfbad0
Michael Pilquist <mpilquist@gmail.com>
2015-01-29 16:23:52 -0500

    Monad laws

---
+++ laws/src/main/scala/structures/laws/MonadLaws.scala
@@ -0,0 +1,56 @@
+package structures
+package laws
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop._
+import org.typelevel.discipline.Laws
+
+object MonadLaws {
+  def apply[F[_]: Monad]: MonadLaws[F] = new MonadLaws[F] {
+    def typeClass = Monad[F]
+  }
+}
+
+trait MonadLaws[F[_]] extends ApplicativeLaws[F] {
+
+  import Monad.Adapter
+
+  implicit def typeClass: Monad[F]
+
+  def monadProperties[A, B, C](implicit
+    arbA: Arbitrary[A],
+    arbAtoFB: Arbitrary[A => F[B]],
+    arbFA: Arbitrary[F[A]],
+    arbBtoFC: Arbitrary[B => F[C]]
+  ) = {
+    val F = Monad[F]
+    import F._
+    Seq(
+      "monad left identity" -> forAll { (a: A, f: A => F[B]) =>
+        pure(a).flatMap(f) == f(a)
+      },
+      "monad right identity" -> forAll { (fa: F[A]) =>
+        fa.flatMap { a => pure(a) } == fa
+      },
+      "monad associativity" -> forAll { (fa: F[A], f: A => F[B], g: B => F[C]) =>
+        fa.flatMap(f).flatMap(g) == fa.flatMap { a => f(a).flatMap(g) }
+      }
+    )
+  }
+
+  def monad(implicit
+    arbFInt: Arbitrary[F[Int]],
+    arbIntToFString: Arbitrary[Int => F[String]],
+    arbStringToFLong: Arbitrary[String => F[Long]],
+    arbFIntToString: Arbitrary[F[Int => String]], 
+    arbFStringToLong: Arbitrary[F[String => Long]]
+  ): RuleSet = new RuleSet {
+    def name = "monad"
+    def bases = Nil
+    def parents = Seq(applicative)
+    def props = monadProperties[Int, String, Long]
+  }
+}
+
+
+
--- laws/src/test/scala/structures/laws/LawTests.scala
+++ laws/src/test/scala/structures/laws/LawTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.FunSuite
 import structures.std._
 
 class LawTests extends FunSuite with Discipline {
-  checkAll("List", ApplicativeLaws[List].applicative)
-  checkAll("Option", ApplicativeLaws[Option].applicative)
+  checkAll("List", MonadLaws[List].monad)
+  checkAll("Option", MonadLaws[Option].monad)
   checkAll("Map", ApplyLaws[Map[Int, ?]].apply)
 }
