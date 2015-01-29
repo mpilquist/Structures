0bb84ae9a82990ce91a2913e2ae3d8e83aece3f9
Michael Pilquist <mpilquist@gmail.com>
2015-01-28 23:44:52 -0500

    Added asum to Alternative, made MonadPlus extend Alternative

--- core/src/main/scala/structures/Alternative.scala
+++ core/src/main/scala/structures/Alternative.scala
@@ -4,6 +4,8 @@ import simulacrum.typeclass
 
 @typeclass trait Alternative[F[_]] extends Applicative[F] with PlusEmpty[F] {
 
+  def asum[G[_]: Foldable, A](gfa: G[F[A]]): F[A] =
+    Foldable[G].fold(gfa)(toMonoid[A])
 }
 
 
--- core/src/main/scala/structures/MonadPlus.scala
+++ core/src/main/scala/structures/MonadPlus.scala
@@ -8,9 +8,9 @@ import simulacrum.typeclass
  * Equivalently, this describes type constructors that have `MonadFilter` instances and additionally
  * define a universally quantified associative operation via the `plus` method.
  */
-@typeclass trait MonadPlus[F[_]] extends MonadFilter[F] with PlusEmpty[F] {
+@typeclass trait MonadPlus[F[_]] extends MonadFilter[F] with Alternative[F] {
 
-  def unite[G[_], A](fga: F[G[A]])(implicit G: Foldable[G]): F[A] =
-    flatMap(fga)(ga => G.foldLeft(ga, empty[A])((acc, a) => plus(acc, pure(a))))
+  def unite[G[_]: Foldable, A](fga: F[G[A]]): F[A] =
+    flatMap(fga)(ga => Foldable[G].foldMap(ga)(a => pure(a))(toMonoid[A]))
 }
 
