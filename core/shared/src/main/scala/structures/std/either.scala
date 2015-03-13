3494b275ffd50cbe3a0958f76e7de286dff5ca31
Michael Pilquist <mpilquist@gmail.com>
2015-03-13 12:19:30 -0400

    Add accumulating applicative instance for either

--- core/shared/src/main/scala/structures/std/either.scala
+++ core/shared/src/main/scala/structures/std/either.scala
@@ -3,13 +3,27 @@ package std
 
 trait either {
 
-  implicit def either[L]: Monad[Either[L, ?]] with Traverse[Either[L, ?]] = new Monad[Either[L, ?]] with Traverse[Either[L, ?]] {
-    def pure[A](a: A) = Right(a)
-    def flatMap[A, B](fa: Either[L, A])(f: A => Either[L, B]) = fa.right.flatMap(f)
+  implicit def eitherTraverse[L]: Traverse[Either[L, ?]] = new Traverse[Either[L, ?]] {
+    def map[A, B](fa: Either[L, A])(f: A => B) = fa.right.map(f)
     def foldLeft[A, B](fa: Either[L, A], initial: B)(f: (B, A) => B) = fa.fold(_ => initial, a => f(initial, a))
     def foldRight[A, B](fa: Either[L, A], initial: B)(f: (A, B) => B) = fa.fold(_ => initial, a => f(a, initial))
     def traverse[G[_]: Applicative, A, B](fa: Either[L, A])(f: A => G[B]): G[Either[L, B]] = {
       fa.right.map(f).fold(l => Applicative[G].pure(Left(l)), gb => Applicative[G].map(gb)(b => Right(b)))
+    }
+  }
+
+  implicit def eitherMonad[L]: Monad[Either[L, ?]] = new Monad[Either[L, ?]] {
+    def pure[A](a: A) = Right(a)
+    def flatMap[A, B](fa: Either[L, A])(f: A => Either[L, B]) = fa.right.flatMap(f)
+  }
+
+  def accumulatingEither[L](implicit L: Semigroup[L]): Applicative[Either[L, ?]] = new Applicative[Either[L, ?]] {
+    def pure[A](a: A) = Right(a)
+    def apply[A, B](fa: Either[L, A])(ff: Either[L, A => B]) = (fa, ff) match {
+      case (Right(a), Right(f)) => Right(f(a))
+      case (l, Right(_)) => l.asInstanceOf[Either[L, B]]
+      case (Right(_), l) => l.asInstanceOf[Either[L, B]]
+      case (Left(x), Left(y)) => Left(L.combine(x, y))
     }
   }
 }
